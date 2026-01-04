4e6aa69907293ed945f7fd4c468765e89af3501b
gustavhallberg <hallberg.gustav@gmail.com>
2026-01-04 19:20:46 +0100

    Added themes, script pending

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,5 +1,6 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-
+set -gx QT_QPA_PLATFORMTHEME qt6ct
+set -gx QT_QPA_PLATFORM wayland
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
